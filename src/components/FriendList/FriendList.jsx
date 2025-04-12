40a0c17c3ca9dc751b24d3c8787811dfe441ce34
Oleksand Tkach <sashatkach66@gmail.com>
2025-04-12 22:10:23 +0300

    message commit

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,29 @@
-import styles from './FriendList.module.css';
-
+import PropTypes from 'prop-types';
 import FriendListItem from '../FriendListItem/FriendListItem';
-
+import { List } from './FriendList.styled';
+  
 const FriendList = ({ friends }) => (
-	<ul className={styles.friendList}>
-		{friends.map(({ id, avatar, name, isOnline }) => (
-			<li key={id}>
-				<FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
-			</li>
-		))}
-	</ul>
+  <List>
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </List>
 );
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Item, Status, Avatar, Name} from './Friendlist.styled';
+import { Item, Status, Avatar, Name} from '../FriendList/FriendList.styled';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
   <Item>  
